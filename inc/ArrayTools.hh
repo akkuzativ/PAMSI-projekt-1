ddc812f598330dd6b0f48bfa5128b436cd0fe873
akkuzativ <goreckialeksander9@gmail.com>
2020-03-14 23:37:28 +0100

    .

--- inc/ArrayTools.hh
+++ inc/ArrayTools.hh
@@ -1,25 +1,37 @@
 #ifndef ARRAYTOOLS_HH
 #define ARRAYTOOLS_HH
 
+#include <iostream>
+#include <random>
 
 
-template <typename T>
-int GetLenght(T * array);
 
-template <typename T>
-int GetMiddleIndex(T * array);
 
-template <typename T>
-bool IsSorted(T * array);
 
-template <typename T>
-void PrintArray(T * array);
+template <typename T> int GetLength(T * array)
+{
+    return *(&array + 1) - array;
+}
 
-template <typename T>
-void FillWithSortedData(T * array);
+template <typename T> int GetMiddleIndex(T * array);
 
-template <typename T>
-void FillWithRandomData(T * array);
+template <typename T> bool IsSorted(T * array);
+
+template <typename T> void PrintArray(T * array)
+{
+    for(int i = 0; array[i]!='\0'; i++)
+    {
+        std::cout << array[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename T> void FillWithSortedData(T * array);
+
+template <typename T> T* CreateArray(int length)
+{
+    return new T[length];
+}
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,13 @@
 
 int main()
 {
+    int N = 5;
 
-    int test_arr[10] = {9, 4, 3, 2, 1, 6, 7, 8, 0, 5};
-    Quicksort(test_arr, '>');
+    //int test_arr[10] = {9, 4, 3, 2, 1, 6, 7, 8, 0, 5};
+
+
+    int* array = CreateArray<int>(N);
+    array[2] = 1;
 
     return 0;
 }
