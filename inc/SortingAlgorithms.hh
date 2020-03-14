d2ba30efc2e176f9c4113e8e2c9b5a4d255f0097
akkuzativ <goreckialeksander9@gmail.com>
2020-03-14 21:33:56 +0100

    .

--- inc/SortingAlgorithms.hh
+++ inc/SortingAlgorithms.hh
@@ -1,20 +1,20 @@
 #ifndef SORTINGALGORITHMS_HH
 #define SORTINGALGORITHMS_HH
 
+#include <iostream>
 
 
-template <typename T>
-void Quicksort(T* array, const char order);
 
-template <typename T>
-void Heapsort(T* array, const char order);
+template <typename T> void Quicksort(T* array, const char order)
+{
+    std::cout << "benin" << std::endl;
+}
 
-template <typename T>
-void Mergesort(T* array, const char order);
+template <typename T> void Heapsort(T* array, const char order);
 
-template <typename T>
-void Introsort(T* array, const char order);
+template <typename T> void Mergesort(T* array, const char order);
 
+template <typename T> void Introsort(T* array, const char order);
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 #include "../inc/ArrayTools.hh"
+#include "../inc/SortingAlgorithms.hh"
 
 int main()
 {
-    std::cout << "projekt-1" << std::endl;
+
+    int test_arr[10] = {9, 4, 3, 2, 1, 6, 7, 8, 0, 5};
+    Quicksort(test_arr, '>');
+
     return 0;
 }
