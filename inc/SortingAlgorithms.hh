1ca9aa0e481f010bc9d80f7c88682ad3c9bce35a
akkuzativ <goreckialeksander9@gmail.com>
2020-03-14 14:37:11 +0100

    .

--- inc/SortingAlgorithms.hh
+++ inc/SortingAlgorithms.hh
@@ -1,4 +1,20 @@
 #ifndef SORTINGALGORITHMS_HH
 #define SORTINGALGORITHMS_HH
 
+
+
+template<typename T>
+void quicksort(T * array);
+
+template<typename T>
+void heapsort(T * array);
+
+template<typename T>
+void mergesort(T * array);
+
+template<typename T>
+void introsort(T * array);
+
+
+
 #endif
